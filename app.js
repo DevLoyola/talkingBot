c47c5f161df4485d215d446a28cef7ef2df93e29
Danielle Loyola <danielle.loyola@developersinstitute.co.nz>
2023-02-12 07:11:16 +1300

    added settings for speech systhesis for robot replies

--- app.js
+++ app.js
@@ -11,7 +11,24 @@ recognition.onstart = function () {
 
 recognition.onresult = function (event) {
   console.log(event);
+  const spokenWords = event.results[0][0].transcript;
+
+  console.log("spoken words are", spokenWords);
+
+  computerSpeech(spokenWords);
 };
+
+function computerSpeech(words) {
+  const speech = new SpeechSynthesisUtterance();
+
+  speech.lang = "en.US";
+  speech.pitch = 0.9;
+  speech.volume = 1;
+  speech.rate = 1;
+  speech.text = words;
+
+  window.speechSynthesis.speak(speech);
+}
 
 button.addEventListener("click", () => {
   recognition.start();
